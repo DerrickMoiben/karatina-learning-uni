9f0078b625eec573454f0dbeec1834e293dfd261
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-09 16:26:16 +0300

    learnt function calling in c++

--- Hello_world/helo.cpp
+++ Hello_world/helo.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
-    cout << "Hello world!" << endl;
+    cout << "Wozaa " << endl;
+    int i, j;
+    int sum;
+    string name;
+
+    cout << "Please enter your name " ;
+    cin >> name;
+
+    cout << "Please enter number one ";
+    cin >> i;
+    
+    cout << "Please enter number two ";
+    cin >> j;
+
+    sum = i + j;
+
+    cout << "The sum of the two numbers is  " << sum << " you name is " << name <<  endl;
+
     return 0;    
 }
