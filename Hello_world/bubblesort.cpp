93ad1916fc533341caf6b3ec2cb8588cbe658985
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-18 18:24:42 +0300

    now we declare the array before sorting

--- Hello_world/bubblesort.cpp
+++ Hello_world/bubblesort.cpp
@@ -14,28 +14,48 @@ void bubblesort(int  data[], int size){
         }
     }
 }
+// This main function prompts the user to enter  the numbers to be sorted
+
+
+// int main(){
+//     int size;
+
+//     cout << "Enter a nuber of array to  be sorted ";
+//     cin >> size;
+
+//     int data[size];
+
+//     cout << "Enter the elements of the array ";
+//     for (int i = 0; i <  size; i++){
+//         cin >> data[i];
+//     }
+
+//     bubblesort(data, size);
+
+//     cout << "Sorted array: ";
+//     for (int i = 0; i < size; i++){
+//         cout << data[i] << " ";
+//     }
+
+//     cout << endl;
+//     return 0;
+
+// }
+
 
 int main(){
     int size;
 
-    cout << "Enter a nuber of array to  be sorted ";
-    cin >> size;
+    int data[] = {7, 5, 7, 3 , 2, 6};
+    size = sizeof(data) / sizeof(data[0]);
 
-    int data[size];
+   bubblesort(data, size);
 
-    cout << "Enter the elements of the array ";
-    for (int i = 0; i <  size; i++){
-        cin >> data[i];
-    }
+   cout << "The sorted array is: ";
+   for (int i = 0; i < size; i++){
+    cout << data[i] << " ";
+   }
+   cout << endl;
 
-    bubblesort(data, size);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < size; i++){
-        cout << data[i] << " ";
-    }
-
-    cout << endl;
     return 0;
-
 }
