9d2e1e3df14efb07c8a04b52f2129a2521138385
Alogarithm_Ke <derrickmoio92@gmail.com>
2024-10-24 08:41:36 +0300

    This is a well detailed binary search study

--- Hello_world/binarysearch.cpp
+++ Hello_world/binarysearch.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int binarysearch(int a[], int l, int r, int x){
-    if (r >= l){
-        int mid = l + (r - l) / 2;
+int binarysearch(int a[], int low, int high, int x){
+    // l is the leftmost index of the array
+    // r is the rightmost index of the array
+    // x is the element to be searched
+
+    if (high >= low){
+        int mid = low + (high - low) / 2;
         if (a[mid] == x){
             return mid;
         }
         if (a[mid] > x){
-            return binarysearch(a, l, mid - 1, x);
+            return binarysearch(a, low, mid - 1, x);
         }
-        return binarysearch(a, mid + 1, r, x);
+        return binarysearch(a, mid + 1, high, x);
     }
-    return -1;
+    else {
+        return -1;
+    }
 }
-
 int main(){
     int a[] = { 2, 3, 4, 10, 40 };
     int n = sizeof(a) / sizeof(a[0]);
     int x = 10;
     int result = binarysearch(a, 0, n - 1, x);
-    if (result == -1){
-        cout << "Element is not present in the array" << endl;
-    }
-    else{
-        cout << "Element is present at index " << result << endl;
-    }
+    cout << "Element is present at index " << result << endl;
     return 0;
 }
+
+// The Best case in this is O(1)
+// The Average case in this is O(log n)
+// The Worst case in this is O(log n)
