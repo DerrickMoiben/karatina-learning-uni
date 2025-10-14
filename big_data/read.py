c79e92181613113a0e4d12de99d7dec7e364ca8d
DerrickMoiben <derrickmoio92@gmail.com>
2025-10-14 19:03:16 +0300

    learning padas in unit og big data analytics

---
+++ big_data/read.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+
+mydataset = {
+    'cars': ["BMW", "VOLVO", "FORD"],
+    'passings': [3, 7, 2]
+}
+
+data = pd.DataFrame(mydataset)
+
+print(data)
+
+#series in pandas is one dimensional array holding data of any type
+print("Break the line here starts the series")
+a = [1, 7, 2]
+
+data1 = pd.Series(a, index = ["x", "y", "z"])
+
+
+print(data1)
+
+print('printing element in index zero')
+print(data1["x"])
+
+
+#using key/value object like a  dictionary
+
+calories = {"day1":420, "day2":380, "day3":390}
+
+data2 =  pd.Series(calories)
+
+print(data2)
+print(data2["day3"])
+
+
+
+#DataFrame
+
+#Dataframe are usually multi-dimensional tables
+
+information = {
+    "calories": [420, 380, 399],
+    "duration": [50, 40, 45]
+}
+
+df =  pd.DataFrame(information)
+
+df =  pd.DataFrame(information, index = ["day1", "day2", "day3"])
+
+print(df)
+
+print(df.loc[["day1", "day2"]])
+
+
+# fro named index in the loc 
+
+
+#load a file of csv as a dataframe
+
+df = pd.read_csv('cinema (1).csv')
+
+pd.options.display.max_rows = 9999
+
+print(df.to_string())
